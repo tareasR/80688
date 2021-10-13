c28b70bfe41f825bc2aed0b308475936e66b45a8
tareasR <tareasrojano@gmail.com>
2021-10-13 14:48:14 -0500

    agregado funcionalidad js para pintar en html los usuarios

--- ProyectoLogeo/index.js
+++ ProyectoLogeo/index.js
@@ -1,3 +1,6 @@
+var id;
+var estado;
+
 var btnLlamar = document.getElementById("llamar")
 btnLlamar.addEventListener("click", function () {
     axios.get("http://localhost:4567/usuario")
@@ -8,32 +11,6 @@ btnLlamar.addEventListener("click", function () {
         console.log(error);
     })
 })
-
-var btnUsuarios = document.getElementById("usuarios")
-btnUsuarios.addEventListener("click", function () {
-    axios.get("http://localhost:4567/usuarios")
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-})
-
-var btnRegistrar = document.getElementById("registrar")
-btnRegistrar.addEventListener("click", function () {
-    axios.post("http://localhost:4567/usuario", {
-        email : document.getElementById("email").value,
-        password : document.getElementById("password").value
-    })
-    .then(function (response) {
-        alert("mensaje: usuario creado "+response.data.status+" con id: "+response.data.id);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-})
-
 
 var btnPagina = document.getElementById("pagina")
 btnPagina.addEventListener("click",function () {
@@ -48,3 +25,49 @@ btnPagina.addEventListener("click",function () {
         console.log(error);
     })
 })
+
+var btnPagina2 = document.getElementById("pagina2")
+btnPagina2.addEventListener("click",function () {
+    let params = new URLSearchParams();
+    params.append("status", estado);
+    params.append("id", id)
+    window.location.replace("http://127.0.0.1:5500/pagina.html?"+params)
+})
+
+var btnUsuarios = document.getElementById("usuarios")
+btnUsuarios.addEventListener("click", function () {
+    axios.get("http://localhost:4567/usuarios")
+    .then(function (response) {
+        console.log(response.data);
+        usuarios(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+var btnRegistrar = document.getElementById("registrar")
+btnRegistrar.addEventListener("click", function () {
+    axios.post("http://localhost:4567/usuario", {
+        email : document.getElementById("email").value,
+        password : document.getElementById("password").value
+    })
+    .then(function (response) {
+        alert("mensaje: usuario creado "+response.data.status+" con id: "+response.data.id);
+        id = response.data.id;
+        estado=response.data.status;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+function usuarios(u) {
+    let listTareas = document.getElementById("tareas")
+    let text = "";
+    for (let i = 0; i < u.length; i++) {
+        let tarea = document.createElement("li");
+        tarea.textContent = u[i].email + " " + u[i].id;
+        listTareas.appendChild(tarea);
+    }
+}
