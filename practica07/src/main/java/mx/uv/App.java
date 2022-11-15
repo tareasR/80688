9bc9732787b7d65b198128edce72e20597ff5e91
ejemplo <clear>
2022-11-15 15:18:00 -0600

    practica 7

--- practica07/src/main/java/mx/uv/App.java
+++ practica07/src/main/java/mx/uv/App.java
@@ -18,6 +18,21 @@ public class App {
 
     public static void main(String[] args) {
         port(80);
+        options("/*", (request, response) -> {
+                String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+                System.out.println(accessControlRequestHeaders);
+                if (accessControlRequestHeaders != null) {
+                    response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                }
+                String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+                System.out.println(accessControlRequestMethod); 
+                if (accessControlRequestMethod != null) {
+                    response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                }
+                return "OK";
+            });
+            
+        before((req, res)-> res.header("Access-Control-Allow-Origin", "*"));
         System.out.println("Hello World!");
         Usuario u1 = new Usuario("1", "Pedro", "1234");
         Usuario u2 = new Usuario("2", "Pablo", "7890");
