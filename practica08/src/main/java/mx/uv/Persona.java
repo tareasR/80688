ad6551ce966d2b31720d2f2c8f65481558fc0c6b
ejemplo <clear>
2022-12-01 14:36:07 -0600

    plantilla

---
+++ practica08/src/main/java/mx/uv/Persona.java
@@ -0,0 +1,17 @@
+package mx.uv;
+
+public class Persona {
+    public String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
